3e0e18a1b47c57108b20459876e36cf39ccca780
wy580477 <98247050+wy580477@users.noreply.github.com>
2025-01-23 11:10:07 +0800

    Update constant.ts

--- app/constant.ts
+++ app/constant.ts
@@ -490,7 +490,7 @@ const googleModels = [
   "gemini-exp-1121",
   "gemini-exp-1206",
   "gemini-2.0-flash-exp",
-  "gemini-2.0-flash-thinking-exp-1219",
+  "gemini-2.0-flash-thinking-exp-01-21",
 ];
 
 const anthropicModels = [
